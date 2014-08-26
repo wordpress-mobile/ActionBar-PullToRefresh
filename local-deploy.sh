80cf3486689a56ab069393ce516049bbe603f234
Maxime Biais <maxime.biais@gmail.com>
2014-08-26 15:27:56 +0200

    fix build and bump version number

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,13 +1,8 @@
 
-VERSION=0.9.5
+VERSION=0.9.7
 GROUP_ID=org.wordpress
 
 # Push to the local maven repo
 mvn install:install-file -Dfile=library/build/libs/library.aar \
     -DgroupId=$GROUP_ID -DartifactId=pulltorefresh-main -Dpackaging=aar \
-    -Dversion=$VERSION
-
-mvn install:install-file \
-    -Dfile=extras/actionbarsherlock/build/libs/extras/actionbarsherlock.aar  \
-    -DgroupId=$GROUP_ID -DartifactId=pulltorefresh-abs -Dpackaging=aar \
     -Dversion=$VERSION
